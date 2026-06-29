ff97bd057c9e2c0fd05f0b4dda33e072601092bc
Thenkuzhali <thenkuzhali13777@gmail.com>
2026-06-29 15:14:09 +0530

    update: adding and updating dictionary

--- adding_and_updating_dicts/adding_and_updating.py
+++ adding_and_updating_dicts/adding_and_updating.py
@@ -48,3 +48,59 @@ print(employee_dictionary)
 
 """ 5. Change all values to a specific value"""
 
+my_dict = {"orange": 20, "apple": 10, "lichy": 10}
+
+for k, v in my_dict.items():
+    my_dict[k]=10
+    
+print(my_dict)
+
+"""6. Insert a key only if it doesn't exist."""
+
+k = "Cherry"
+v = 40
+
+if k not in my_dict:
+    my_dict[k]=v
+    
+print(my_dict)
+
+
+"""7. Add a key with a default value."""
+
+my_dict.setdefault("grapes", 10)
+print(my_dict)
+
+"""8. Merge two dictionaries."""
+
+dict1 = {"name": "Peter"}
+dict2 = {"age": 20}
+
+dict1 |= dict2
+print(dict1)
+
+dict3 = {"name": "Shreya"}
+dict4 = {"age": 21}
+
+merged = dict3 | dict4
+print(merged)
+
+dict5 = {"name": "Sharan"}
+dict6 = {"age": 22}
+
+merged1 = {**dict5, **dict6}
+print(merged1)
+
+"""9. Replace a value based on a condition."""
+for k, v in my_dict.items():
+    if v==10:
+        my_dict[k]=20
+print(my_dict)
+
+"""10. Update nested dictionary values."""
+data = {"user": {"id": 101, "info": {"name": "Alice", "role": "Admin"}}}
+
+# Updates "role" and adds "status" inside the "info" dictionary
+data["user"]["info"].update({"role": "Superadmin", "status": "active"})
+
+print(data)
