71d2f287d36d5e9740fa899900dcc1b1e41329e1
Thenkuzhali <thenkuzhali13777@gmail.com>
2026-07-02 13:35:34 +0530

    remove keys and values with conditions

--- Deletion Operations/delete_operations.py
+++ Deletion Operations/delete_operations.py
@@ -45,12 +45,39 @@ cleaned = {k: v for k, v in fruit_dict.items() if v}
 print(cleaned)
 
 """7. Delete a nested key."""
+data = {"user": {"profile": {"name": "Sam", "age": 30, "status": "active"}}}
+print(data)
 
+del data["user"]["profile"]["age"]
+print("after del: ",data)
+
+del_v = data["user"]["profile"].pop("status")
+print(del_v)
+
+print("after pop: ",data)
 
 """8. Remove duplicate values."""
+skincare_products = {"face wash": "derma-co", "sun screen": "derma-co", "moisturiser": "dot and key", "body lotion":"nivea"}
 
+print(skincare_products)
+seen = set()
+cleaned_prod = {k: v for k, v in skincare_products.items() if v not in seen and not seen.add(v)}
+
+print(cleaned_prod)
 
 """9. Remove keys starting with a specific letter."""
 
+remove_starts_with_n = {}
+for k, v in skincare_products.items():
+    if k.startswith("f"):
+        continue
+    remove_starts_with_n.update({k: v})
+print(remove_starts_with_n)
 
 """10. Remove keys with values less than 10."""
+  
+squares = {'1': 1, '2': 4, '3': 9, '4': 16, '5':25}
+ 
+less_than_ten = {k: v for k, v in squares.items() if not v<10}
+
+print(less_than_ten)
