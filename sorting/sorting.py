88f230d6efbd82befbd1efce6220c91c8f4d366c
Thenkuzhali <thenkuzhali13777@gmail.com>
2026-08-14 00:21:14 +0530

    Add Sorting in dictionary

---
+++ sorting/sorting.py
@@ -0,0 +1,29 @@
+"""
+1. Sort dictionary by keys.
+2. Sort dictionary by values.
+3. Sort dictionary by key in descending order.
+4. Sort dictionary by value in descending order.
+5. Get top 3 highest values.
+6. Get top 3 lowest values.
+7. Sort nested dictionaries.
+8. Sort dictionary items alphabetically.
+9. Sort by length of values.
+10. Sort a dictionary containing string values.
+"""
+
+# 1. Sort dictionary by keys.
+
+
+# 2. Sort dictionary by values.
+
+
+#3. Sort dictionary by key in descending order.
+
+
+#4. Sort dictionary by value in descending order.
+#5. Get top 3 highest values.
+#6. Get top 3 lowest values.
+#7. Sort nested dictionaries.
+#8. Sort dictionary items alphabetically.
+#9. Sort by length of values.
+#10. Sort a dictionary containing string values.
